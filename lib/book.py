4c9f3cdee33204bfcc0535a59c6520db9664cf8b
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-12-01 21:16:05 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -2,5 +2,21 @@
 
 class Book:
     pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     
-        
+    page_count = property(get_page_count, set_page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,23 @@
 
 class Shoe:
     pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = "New"
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    
+    size = property(get_size, set_size)
